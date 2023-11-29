35522636138b97eae15eba880a45d9dfbd1cce90
ValentinaGuryanova <gvalushka@mail.ru>
2023-11-29 20:12:30 +0300

    24.2 Доработка

--- education/serliazers.py
+++ education/serliazers.py
@@ -31,7 +31,7 @@ class CourseSerializer(serializers.ModelSerializer):
     lessons = LessonSerializer(source='lesson_set', read_only=True, many=True)
 
     def get_lesson_count(self, obj):
-        return obj.lesson.count()
+        return Lesson.objects.filter(course=obj.id).count()
 
     def get_lessons_of_course(self, obj):
         return LessonSerializer(Lesson.objects.filter(course=obj), many=True).data
