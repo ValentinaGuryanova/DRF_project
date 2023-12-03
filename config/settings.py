162ca6772feea228a0164c78e4f4537eabd51de7
ValentinaGuryanova <gvalushka@mail.ru>
2023-12-04 01:29:40 +0300

    26.2. Celery

--- config/settings.py
+++ config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     'drf_yasg',
     'corsheaders',
+    'django_celery_beat',
     #'docs',
 
     'users',
@@ -160,4 +161,19 @@ CORS_ALLOWED_ORIGINS = [
 CSRF_TRUSTED_ORIGINS = [
     "https://read-and-write.example.com",
 ]
+
+# URL-адрес брокера сообщений
+CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0' # Например, Redis, который по умолчанию работает на порту 6379
+
+# URL-адрес брокера результатов, также Redis
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/0'
+
+# Часовой пояс для работы Celery
+CELERY_TIMEZONE = "Europe/Moscow"
+
+# Флаг отслеживания выполнения задач
+CELERY_TASK_TRACK_STARTED = True
+
+# Максимальное время на выполнение задачи
+CELERY_TASK_TIME_LIMIT = 30 * 60
 
--- users/serializers.py
+++ users/serializers.py
@@ -1,7 +1,40 @@
 from rest_framework import serializers
 
+from education.models import Payment
 from users.models import Subscription
 from users.validators import AlreadySubscribedCheck
+
+from rest_framework import serializers
+
+from users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """ Класс сериализатора для модели User """
+
+    payments = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = '__all__'
+        # fields = ('email', 'phone', 'avatar', 'city', 'is_active', 'payments')
+
+    def get_payments(self, user):
+        """ Метод определения поля payments, возвращает список платежей пользователя """
+
+        payments = []
+        for payment in Payment.objects.filter(payment_user=user):
+            pay = [payment.payment_date, payment.amount]
+            payments.append(pay)
+        return payments
+
+
+class AnyUserSerializer(serializers.ModelSerializer):
+    """ Класс сериализатора для модели User при использовании стронним пользователем """
+
+    class Meta:
+        model = User
+        fields = ('id', 'email', 'city')
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
